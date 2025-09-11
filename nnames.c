e5fd62cd9f4654d20b14b820a634b3b1ba2a1ac1
Ashwani  Kumar Mishra <mishraashwani305@gmail.com>
2025-09-11 12:04:20 +0530

    Add files via upload

---
+++ nnames.c
@@ -0,0 +1,13 @@
+//to print name n times
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("no.of times you want to print Soumya");
+scanf("%d",&n);
+for(i=1; i<n; i++)
+  {
+  printf("soumya vats \n");
+  }
+
+}
